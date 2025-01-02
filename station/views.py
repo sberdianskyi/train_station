18de1875bac7e90992f05a2286673f20a81bcc93
Serhii Berdianskyi <zeferot94@gmail.com>
2025-01-02 11:36:30 +0200

    added StationViewSet

--- station/views.py
+++ station/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from station.models import Station
+from station.serializers import StationSerializer
+
+
+class StationViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
