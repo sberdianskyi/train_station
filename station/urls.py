f580b1c56ccbc3cc7f4cde1ac4c6959e6fd79a1a
Serhii Berdianskyi <zeferot94@gmail.com>
2025-01-05 18:20:44 +0200

    added route for Route

--- station/urls.py
+++ station/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from station.views import StationViewSet
+from station.views import (
+    StationViewSet,
+    RouteViewSet,
+    TrainTypeViewSet,
+    TrainViewSet,
+    CrewViewSet,
+    JourneyViewSet
+)
 
 router = routers.DefaultRouter()
-router.register("station", StationViewSet)
+router.register("stations", StationViewSet)
+router.register("routes", RouteViewSet)
+
 
 urlpatterns = [path("", include(router.urls))]
 
