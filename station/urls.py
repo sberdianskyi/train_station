0d54ceacc25545f109b126a5f70d9890784bc151
Serhii Berdianskyi <zeferot94@gmail.com>
2025-01-05 18:22:42 +0200

    added route for Train

--- station/urls.py
+++ station/urls.py
@@ -5,12 +5,14 @@ from station.views import (
     StationViewSet,
     RouteViewSet,
     TrainTypeViewSet,
+    TrainViewSet,
 )
 
 router = routers.DefaultRouter()
 router.register("stations", StationViewSet)
 router.register("routes", RouteViewSet)
 router.register("train_types", TrainTypeViewSet)
+router.register("trains", TrainViewSet)
 
 
 urlpatterns = [path("", include(router.urls))]
