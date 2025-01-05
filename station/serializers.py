65c04fe79f027efc1d61ae4a342c5cd38f912834
Serhii Berdianskyi <zeferot94@gmail.com>
2025-01-05 18:08:58 +0200

    added Route serializer

--- station/serializers.py
+++ station/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 
-from station.models import Station
+from station.models import Station, TrainType, Route, Train, Crew, Journey
 
 
 class StationSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Station
         fields = ("id", "name", "latitude", "longitude")
-        read_only_fields = ("id",)
+
+
+class RouteSerializer(serializers.ModelSerializer):
+    source = StationSerializer(many=False, read_only=True)
+    destination = StationSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Route
+        fields = ("id", "source", "destination", "distance")
