7b92a3cd25338c38bc09af64142d69ad4fca52d3
Serhii Berdianskyi <zeferot94@gmail.com>
2025-01-05 18:14:31 +0200

    added RouteViewSet

--- station/views.py
+++ station/views.py
@@ -1,8 +1,9 @@
 from rest_framework import mixins
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
-from station.models import Station
-from station.serializers import StationSerializer
+from station.models import Station, Route, TrainType, Train, Crew, Journey
+from station.serializers import StationSerializer, RouteSerializer, TrainTypeSerializer, TrainSerializer, \
+    CrewSerializer, JourneySerializer
 
 
 class StationViewSet(
@@ -12,3 +13,12 @@ class StationViewSet(
 ):
     queryset = Station.objects.all()
     serializer_class = StationSerializer
+
+
+class RouteViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
