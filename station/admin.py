95996aa1bb487e7aa1bbcef103dd7b49d2002114
Serhii Berdianskyi <zeferot94@gmail.com>
2025-01-22 10:10:09 +0200

    registered Order and Ticket models in admin file

--- station/admin.py
+++ station/admin.py
@@ -6,7 +6,9 @@ from station.models import (
     TrainType,
     Train,
     Crew,
-    Journey
+    Journey,
+    Order,
+    Ticket
 )
 
 admin.site.register(Station)
@@ -15,3 +17,5 @@ admin.site.register(TrainType)
 admin.site.register(Train)
 admin.site.register(Crew)
 admin.site.register(Journey)
+admin.site.register(Order)
+admin.site.register(Ticket)
