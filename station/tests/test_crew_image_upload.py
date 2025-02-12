94e8e628f1ab1b41e8d2a32af2092f12a17df977
Serhii Berdianskyi <zeferot94@gmail.com>
2025-02-12 11:19:03 +0200

    added tests for Crew image upload

---
+++ station/tests/test_crew_image_upload.py
@@ -0,0 +1,68 @@
+import tempfile
+import os
+
+from PIL import Image
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from station.models import Crew
+
+CREW_URL = reverse("station:crew-list")
+
+def test_crew(**params) -> Crew:
+    default_crew = {"first_name": "Sheldon", "last_name": "Cooper"}
+    default_crew.update(params)
+    return Crew.objects.create(**default_crew)
+
+def image_upload_url(crew_id):
+    """Return URL for recipe image upload"""
+    return reverse("station:crew-upload-image", args=[crew_id])
+
+
+class CrewImageUploadTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="sample@test.com", password="test_password", is_staff=True
+        )
+        self.client.force_authenticate(self.user)
+        self.crew = test_crew()
+
+    def tearDown(self):
+        self.crew.image.delete()
+
+    def test_upload_image_for_crew(self):
+        """Test uploading an image to movie"""
+        url = image_upload_url(self.crew.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            res = self.client.post(url, {"image": ntf}, format="multipart")
+        self.crew.refresh_from_db()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn("image", res.data)
+        self.assertTrue(os.path.exists(self.crew.image.path))
+
+    def test_upload_image_bad_request(self):
+        """Test uploading an invalid image"""
+        url = image_upload_url(self.crew.id)
+        res = self.client.post(url, {"image": "not image"}, format="multipart")
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_image_url_is_shown_on_crew_list(self):
+        url = image_upload_url(self.crew.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            self.client.post(url, {"image": ntf}, format="multipart")
+        res = self.client.get(CREW_URL)
+
+        self.assertIn("image", res.data[0].keys())
