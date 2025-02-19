8df1467da187647dabafd1cc89d732ffbb94e3d6
Serhii Berdianskyi <zeferot94@gmail.com>
2025-02-19 11:21:25 +0200

    updated settings.py and .env_example

--- train_station/settings.py
+++ train_station/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4w1y9w+^!kgli#a22+uanptdj!goq9emov=-1u=2ud)8l849$("
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DEBUG"]
 
 ALLOWED_HOSTS = []
 
